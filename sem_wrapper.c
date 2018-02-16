49439c2323d84f0ffb065df8111b5c1222278496
Joseph <cutinoj@mail.gvsu.edu>
2018-02-16 09:41:02 -0500

    created local .h and .c wrapper for sem

--- sem_wrapper.c
+++ sem_wrapper.c
@@ -1,6 +1,11 @@
 #include <sys/sem.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include "sem_wrapper.h"
 
 
