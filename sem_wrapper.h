4d35ee92e5b112d31e46b67479a2c3fd8437e8f0
Joseph <cutinoj@mail.gvsu.edu>
2018-02-16 10:27:41 -0500

    test

--- sem_wrapper.h
+++ sem_wrapper.h
@@ -9,11 +9,11 @@ union semun
 	struct seminfo* __buf; // buffer for IPC_INFO
 };
 
-void sem_wait(int semId, int num)
+void sem_wait(int semId, int num);
 
-void sem_signal(int semId, int num)
+void sem_signal(int semId, int num);
 
-int sem_create(int num, key_t semkey, unsigned short *start_val)
+int sem_create(int num, key_t semkey, unsigned short *start_val);
 
 void sem_delete(int semId);
 
